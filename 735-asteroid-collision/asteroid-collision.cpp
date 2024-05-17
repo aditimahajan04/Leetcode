0c18ccceb45493a4185833799f7de1bce1f56b37
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2024-05-17 17:55:52 +0530

    Time: 4 ms (94.25%) | Memory: 19.9 MB (93.23%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> ans;
+        for(int &a:asteroids){
+            while(!ans.empty() && ans.back()>0 && a<0){
+                int sum=a+ans.back();
+                if(sum<0){
+                    ans.pop_back();
+                }
+                else if(sum>0){
+                    a=0;
+                }
+                else{
+                    ans.pop_back();
+                    a=0;
+                }
+            }
+            if(a!=0){
+                ans.push_back(a);
+                a=0;
+            }
+        }
+        return ans;
+    }
+};
