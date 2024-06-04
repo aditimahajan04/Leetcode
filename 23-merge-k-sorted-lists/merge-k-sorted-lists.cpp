eabe9fe05524df694c017b9c79c578fe935cc371
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2024-06-04 20:25:09 +0530

    Time: 15 ms (67.82%) | Memory: 16.8 MB (86.17%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,33 @@
+class cmp {
+public:
+    bool operator()(const ListNode* a, const ListNode* b) {
+        return a->val > b->val;
+    }
+};
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* head = new ListNode(0);
+        ListNode* temp = head;
+        priority_queue<ListNode*, vector<ListNode*>, cmp> minHeap;
+
+        int n = lists.size();
+        for (int i = 0; i < n; i++) {
+            if (lists[i] != NULL) {
+                minHeap.push(lists[i]);
+            }
+        }
+        while (!minHeap.empty()) {
+            auto lowest = minHeap.top();
+            minHeap.pop();
+
+            temp->next = lowest;
+            temp = temp->next; 
+            if (lowest->next) {
+                minHeap.push(lowest->next);
+            }
+        }
+        return head->next;
+    }
+};
