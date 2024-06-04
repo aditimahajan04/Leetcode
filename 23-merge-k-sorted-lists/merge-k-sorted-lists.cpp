6a3861d5ea72e135c3655d11a323ea7098690d4f
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2024-06-04 20:30:26 +0530

    Time: 16 ms (62.01%) | Memory: 16.8 MB (86.17%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,30 +1,37 @@
-class cmp {
-public:
-    bool operator()(const ListNode* a, const ListNode* b) {
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class cmp{
+    public:
+    bool operator()(const ListNode *a,const ListNode *b){
         return a->val > b->val;
     }
 };
-
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode* head = new ListNode(0);
-        ListNode* temp = head;
-        priority_queue<ListNode*, vector<ListNode*>, cmp> minHeap;
-
-        int n = lists.size();
-        for (int i = 0; i < n; i++) {
-            if (lists[i] != NULL) {
+        ListNode* head=new ListNode(0);
+        ListNode* temp=head;
+        priority_queue<ListNode*,vector<ListNode*>,cmp> minHeap;
+        int n=lists.size();
+        for(int i=0;i<n;i++){
+            if(lists[i]!=NULL){
                 minHeap.push(lists[i]);
             }
         }
-        while (!minHeap.empty()) {
-            auto lowest = minHeap.top();
+        while(!minHeap.empty()){
+            auto lowest=minHeap.top();
             minHeap.pop();
-
-            temp->next = lowest;
-            temp = temp->next; 
-            if (lowest->next) {
+            temp->next=lowest;
+            temp=temp->next;
+            if(lowest->next){
                 minHeap.push(lowest->next);
             }
         }
