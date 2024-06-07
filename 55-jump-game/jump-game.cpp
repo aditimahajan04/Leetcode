edd538da4c953f3eb23984df359afe4c8ecb36e4
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2024-06-08 00:00:21 +0530

    Time: 48 ms (41.81%) | Memory: 50.7 MB (81.16%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int idxToReach = n - 1;
+        for(int i = n - 1; i >= 0; i--) {
+            if(i + nums[i] >= idxToReach) idxToReach = i;
+        }
+        return idxToReach == 0;
+    }
+};
