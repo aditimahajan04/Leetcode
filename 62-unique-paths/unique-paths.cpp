5b9d0e8ecdac82537a10045c87a5672dcd9856de
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2024-07-04 19:09:22 +0530

    Time: 3 ms (33.45%) | Memory: 7.3 MB (72.15%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> prev(n, 0);
-
-    // Iterate through the rows of the grid.
-    for (int i = 0; i < m; i++) {
-        // Create a temporary vector to represent the current row.
-        vector<int> temp(n, 0);
-
-        // Iterate through the columns of the grid.
-        for (int j = 0; j < n; j++) {
-            // Base case: If we are at the top-left cell (0, 0), there is one way.
-            if (i == 0 && j == 0) {
-                temp[j] = 1;
-                continue;
+        vector<int> prev(n,0);
+        for(int i=0;i<m;i++){
+            vector<int> temp(n,0);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    temp[j]=1;
+                    continue;
+                }
+                int up=0;
+                int left=0;
+                if(i>0){
+                    up=prev[j];
+                }
+                if(j>0){
+                    left=temp[j-1];
+                }
+                temp[j]=up+left;
             }
-
-            // Initialize variables to store the number of ways from the cell above (up) and left (left).
-            int up = 0;
-            int left = 0;
-
-            // If we are not at the first row (i > 0), update 'up' with the value from the previous row.
-            if (i > 0)
-                up = prev[j];
-
-            // If we are not at the first column (j > 0), update 'left' with the value from the current row.
-            if (j > 0)
-                left = temp[j - 1];
-
-            // Calculate the number of ways to reach the current cell by adding 'up' and 'left'.
-            temp[j] = up + left;
+            prev=temp;
         }
-
-        // Update the previous row with the values calculated for the current row.
-        prev = temp;
+        return prev[n-1];
     }
-
-    // The result is stored in the last cell of the previous row (n-1).
-    return prev[n - 1];
-}
 };
