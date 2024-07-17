4362b31d5ff86bb7ebae4d75a29ff34cb04dfabd
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2024-07-17 16:36:35 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (47.02%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,47 +1,49 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-const int mod = (int)1e9 + 7;
+    int mod = 1e9 + 7;
 
-// Function to count the number of ways to achieve the target sum
-int findWays(vector<int> &num, int tar) {
-    int n = num.size();
-
-    vector<int> prev(tar + 1, 0);
-
-    if (num[0] == 0)
-        prev[0] = 2;  // 2 cases - pick and not pick
-    else
-        prev[0] = 1;  // 1 case - not pick
-
-    if (num[0] != 0 && num[0] <= tar)
-        prev[num[0]] = 1;  // 1 case - pick
-
-    for (int ind = 1; ind < n; ind++) {
-        vector<int> cur(tar + 1, 0);
-        for (int target = 0; target <= tar; target++) {
-            int notTaken = prev[target];
-
-            int taken = 0;
-            if (num[ind] <= target)
-                taken = prev[target - num[ind]];
-
-            cur[target] = (notTaken + taken) % mod;
-        }
-        prev = cur;
-    }
-    return prev[tar];
-}
     int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        
         int totSum = 0;
-        int n=nums.size();
         for (int i = 0; i < n; i++) {
-           totSum += nums[i];
-    }
+            totSum += nums[i];
+        }
 
-    // Checking for edge cases
-    if (totSum - target < 0 || (totSum - target) % 2 != 0)
-        return 0;  // Not possible to achieve the target sum
+        // Check if it is possible to partition the array
+        if (totSum - target < 0 || (totSum - target) % 2 != 0) {
+            return 0;
+        }
 
-    return findWays(nums, (totSum - target) / 2);
+        int sum = (totSum - target) / 2;
+
+        // Initialize the dp array with size sum + 1
+        vector<int> prev(sum + 1, 0);
+        if (nums[0] == 0) {
+            prev[0] = 2; // Two ways to make sum 0: +0 and -0
+        } else {
+            prev[0] = 1; // One way to make sum 0: not taking any element
+        }
+        if (nums[0] != 0 && nums[0] <= sum) {
+            prev[nums[0]] = 1; // One way to make sum nums[0]
+        }
+
+        for (int ind = 1; ind < n; ind++) {
+            vector<int> cur(sum + 1, 0);
+            for (int tar = 0; tar <= sum; tar++) {
+                int notTaken = prev[tar];
+
+                int taken = 0;
+                if (nums[ind] <= tar) {
+                    taken = prev[tar - nums[ind]];
+                }
+                cur[tar] = (notTaken + taken) % mod;
+            }
+            prev = cur;
+        }
+        return prev[sum];
     }
 };
