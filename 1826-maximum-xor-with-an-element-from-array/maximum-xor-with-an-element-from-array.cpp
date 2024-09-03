9a7ab651d712f8a58955e04aa8c8a24db1894868
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2024-09-03 12:58:00 +0530

    Time: 531 ms (61.65%) | Memory: 220.4 MB (80.83%) - LeetSync

--- 1826-maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.cpp
+++ 1826-maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.cpp
@@ -1,5 +1,3 @@
-
-
 struct Node {
     Node *links[2];
     
@@ -56,51 +54,30 @@ public:
 
         return maxNum;
     }
-
-    // Now, maximizeXor is part of the Solution class
     vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
         vector<int> ans(queries.size(), 0); 
 
-        // Vector to store offline queries
         vector<pair<int, pair<int, int>>> offlineQueries;
-
-        // Sort the array of numbers
         sort(nums.begin(), nums.end()); 
 
         int index = 0;
-
-        // Convert queries to offline queries and store them in a vector
         for(auto &it: queries) {
             offlineQueries.push_back({it[1], {it[0], index++}});
         }
-
-        // Sort offline queries based on their end points
         sort(offlineQueries.begin(), offlineQueries.end());
-
-        // Pointer to iterate through the array of numbers
         int i = 0; 
-
-        // Number of elements in the array
         int n = nums.size(); 
-
-        // Process each offline query
         for(auto &it : offlineQueries) {
-            // Insert numbers into the trie until the current query's end point
             while(i < n && nums[i] <= it.first) {
                 insert(nums[i]); 
                 i++; 
             }
-
-            // If there are numbers inserted into the trie, find the maximum XOR value for the query range
             if(i != 0) {
                 ans[it.second.second] = findMax(it.second.first);
             } else {
-                // If no numbers inserted, set result to -1
                 ans[it.second.second] = -1; 
             }
         }
-
-        // Return the results of all queries
         return ans;  
     }
 };
