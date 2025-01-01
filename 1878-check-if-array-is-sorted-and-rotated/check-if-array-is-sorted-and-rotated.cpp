139a37632350ca2d5e06883c40697962ff80da08
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2025-01-01 22:29:16 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (56.84%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,15 +1,23 @@
+
 class Solution {
 public:
     bool check(vector<int>& nums) {
-       int n = nums.size();
-        int count = 0;
 
-        for(int i=0; i<n; i++)
-        {   
-            if(nums[i] > nums[(i+1)%n])
-                count ++;
+        int count =0;
+        int n= nums.size();
+
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i]){
+                count++;            // means count 1 ho jayega agr rotated array hua
+            }
         }
-        return (count <= 1);
+        if(nums[n-1]>nums[0]){      // means not rotated sorted in any way !
+            count++;        // count ki value = 2 ho jayegi agr yeh hogyaaa!
+        }
+
+        return count <= 1;        // max count can be 1 for rotated sorted array!!
         
     }
+
+
 };
