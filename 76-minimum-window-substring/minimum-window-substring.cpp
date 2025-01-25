c9375b2791de60cd30b7797e4e646041b952a21e
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2025-01-25 19:09:24 +0530

    Time: 1 ms (86.52%) | Memory: 11.3 MB (92.32%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,21 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int> map(128,0);
-        for(auto c : t){
-            map[c]++;
-        }
-        int end=0, begin=0, head=0, d=INT_MAX, counter=t.size();
+        // Frequency map to track characters in 't'
+        vector<int> freq(128, 0);
+        for (char c : t) freq[c]++; // Count occurrences of each character in 't'
+        
+        int n = s.size(), m = t.size();
+        int left = 0, right = 0; // Pointers for the sliding window
+        int start = 0;           // Start index of the minimum window
+        int minLen = INT_MAX;    // Length of the minimum window
+        int count = m;           // Number of characters in 't' yet to be matched
 
-        while(end<s.size()){
-            if(map[s[end++]]-- > 0) counter--;
-            while(counter==0){
-                if(end-begin<d){
-                    d=end-(head=begin);
+        // Expand the window by moving the 'right' pointer
+        while (right < n) {
+            // If the current character is needed (exists in 't'), decrease the count
+            if (freq[s[right]] > 0) count--;
+
+            // Decrease the frequency of the current character
+            freq[s[right]]--;
+            right++; // Expand the window
+
+            // When the window contains all characters of 't'
+            while (count == 0) {
+                // Update the minimum window if the current one is smaller
+                if (right - left < minLen) {
+                    minLen = right - left; // Update the minimum length
+                    start = left;         // Update the start index
                 }
-                if(map[s[begin++]]++ == 0) counter++;
+
+                // Contract the window by moving the 'left' pointer
+                freq[s[left]]++; // Restore the frequency of the left character
+                if (freq[s[left]] > 0) count++; // If it's needed, increase count
+                left++; // Move the left pointer to shrink the window
             }
         }
-        return d==INT_MAX ? "":s.substr(head,d);
+
+        // If no valid window was found, return an empty string
+        return minLen == INT_MAX ? "" : s.substr(start, minLen);
     }
 };
