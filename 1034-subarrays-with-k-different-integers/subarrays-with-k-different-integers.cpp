5683e67ddfcfb81c70f4c8fab1b70d8c6358edb0
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2025-03-30 17:42:04 +0530

    Time: 95 ms (30.77%) | Memory: 55.4 MB (75.46%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,31 +1,24 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int func(vector<int>& nums, int k) {
-        if (k == 0) return 0; // Edge case: If k is 0, return 0 (no valid subarray)
-
+        if (k == 0) return 0; 
         int l = 0, r = 0, cnt = 0;
         unordered_map<int, int> mpp;
-
         while (r < nums.size()) {
-            mpp[nums[r]]++;  // Expand window by adding nums[r]
+            mpp[nums[r]]++;  
 
-            while (mpp.size() > k) {  // Shrink window if unique elements exceed k
+            while (mpp.size() > k) { 
                 mpp[nums[l]]--;
                 if (mpp[nums[l]] == 0) {
                     mpp.erase(nums[l]);
                 }
-                l++;  // Move left pointer forward
+                l++;  
             }
-
-            cnt += (r - l + 1);  // Count subarrays ending at r
+            cnt += (r - l + 1); 
             r++;
         }
         return cnt;
     }
-
     int subarraysWithKDistinct(vector<int>& nums, int k) {
         return func(nums, k) - func(nums, k - 1);
     }
