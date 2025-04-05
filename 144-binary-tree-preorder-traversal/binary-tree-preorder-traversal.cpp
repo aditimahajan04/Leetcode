bcaafbe455b8569e161a7ef8b14964d3b3125054
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2025-04-05 18:38:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (43.43%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,25 +11,18 @@
  */
 class Solution {
 public:
+void preorder(TreeNode* root, vector<int> &v) {
+        if (root == NULL) {
+            return;
+        }
+        // Postorder traversal: left, right, root
+        v.push_back(root->val);
+        preorder(root->left, v);     // Traverse the left subtree
+        preorder(root->right, v);    // Traverse the right subtre      // Visit the root
+    }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> preorder;
-        if(root==NULL){
-            return preorder;
-        }
-
-        stack<TreeNode*>st;
-        st.push(root);
-        while(!st.empty()){
-            root=st.top();
-            st.pop();
-            preorder.push_back(root->val);
-            if(root->right!=NULL){
-                st.push(root->right);
-            }
-            if(root->left!=NULL){
-                st.push(root->left);
-            }
-        }
-        return preorder;
+        vector<int> v;
+        preorder(root, v);           // Call the recursive helper function
+        return v;
     }
 };
