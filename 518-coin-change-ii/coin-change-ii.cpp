38c087ad5d824ffa679be386dc2efa75a9fdc3c3
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2025-05-03 19:49:20 +0530

    Time: 15 ms (74.70%) | Memory: 10.7 MB (76.16%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<int> prev(amount+1,0);
+        int n = coins.size();
+        using ULL = unsigned long long;  // alias for brevity
+        vector<ULL> prev(amount + 1, 0), cur(amount + 1, 0);
 
-        for(int i=0;i<=amount;i++){
-            if(i%coins[0]==0){
-                prev[i]=1;
-            }
+        for (int T = 0; T <= amount; T++) {
+            prev[T] = (T % coins[0] == 0);
         }
-        for(int ind=1;ind<n;ind++){
-            vector<int> cur(amount+1,0);
-            for(int target=0;target<=amount;target++){
-                int notTaken=prev[target];
-                int taken=0;
-                if(coins[ind]<=target){
-                    taken=cur[target-coins[ind]];
+
+        for (int ind = 1; ind < n; ind++) {
+            for (int T = 0; T <= amount; T++) {
+                ULL notTake = prev[T];
+                ULL take = 0;
+                if (coins[ind] <= T) {
+                    take = cur[T - coins[ind]];
                 }
-                cur[target]=notTaken+taken;
+                cur[T] = take + notTake;
             }
-            prev=cur;
+            prev = cur;
         }
-        return prev[amount];
+
+        return static_cast<int>(prev[amount]);  // cast back to int safely
     }
 };
