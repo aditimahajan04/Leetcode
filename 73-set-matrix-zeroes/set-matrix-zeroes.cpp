2277ccaba9ad89aec60d36000cf0d258a34be32d
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2025-05-04 23:01:52 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (52.59%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int col0=1;
-        int rows=matrix.size();
-        int cols=matrix[0].size();
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int col0 = 1;
 
-        for(int i=0;i<rows;i++){
-            if(matrix[i][0]==0){
-                col0=0;
-            }
-            for(int j=1;j<cols;j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=matrix[0][j]=0;
+        // First pass: use first row and col as markers
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(int i=rows-1;i>=0;i--){
-            for(int j=cols-1;j>=1;j--){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
-                    matrix[i][j]=0;
-                }
+
+        // Second pass: zero cells based on markers
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
             }
-            if(col0==0){
-                matrix[i][0]=0;
-            }
+        }
+
+        // Third pass: zero first row and column if needed
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++)
+                matrix[0][j] = 0;
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++)
+                matrix[i][0] = 0;
         }
     }
 };
