f52440b00aa366489319ce79854237c11d80678e
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2025-05-11 12:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (8.52%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        int num1=-1,num2=-1,c1=0,c2=0,i;
-        for(int i=0;i<n;i++){
-            if(nums[i]==num1){
-                c1++;
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = 0, el2 = 1;  // use different dummy values initially
+
+        for (int num : nums) {
+            if (num == el1) cnt1++;
+            else if (num == el2) cnt2++;
+            else if (cnt1 == 0) {
+                el1 = num;
+                cnt1 = 1;
             }
-            else if(nums[i]==num2){
-                c2++;
+            else if (cnt2 == 0) {
+                el2 = num;
+                cnt2 = 1;
             }
-            else if(c1==0){
-                num1=nums[i];
-                c1++;
-            }
-            else if(c2==0){
-                num2=nums[i];
-                c2++;
-            }
-            else{
-                c1--;
-                c2--;
+            else {
+                cnt1--;
+                cnt2--;
             }
         }
-        vector<int> ans;
-        c1=c2=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==num1){
-                c1++;
-            }
-            else if(nums[i]==num2){
-                c2++;
-            }
+
+        // Verify actual counts
+        cnt1 = 0, cnt2 = 0;
+        for (int num : nums) {
+            if (num == el1) cnt1++;
+            else if (num == el2) cnt2++;  // use else-if to avoid double-counting if el1 == el2
         }
-        if(c1>n/3){
-            ans.push_back(num1);
-        }
-        if(c2>n/3){
-            ans.push_back(num2);
-        }
-        return ans;
+
+        int n = nums.size();
+        vector<int> result;
+        if (cnt1 > n / 3) result.push_back(el1);
+        if (cnt2 > n / 3) result.push_back(el2);
+
+        sort(result.begin(), result.end());  // optional if sorted output is needed
+        return result;
     }
 };
