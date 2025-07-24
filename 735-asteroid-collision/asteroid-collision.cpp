702c29dce57c7b5432438afa600d57fd02d68ec7
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2025-07-25 02:23:33 +0530

    Time: 4 ms (16.02%) | Memory: 23.3 MB (5.27%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        int n = asteroids.size();
-        list<int> st; // Changed from stack to list
+        int n=asteroids.size();
+        list<int> st;
 
-        for (int i = 0; i < n; i++) { // Loop should be till n, not n-1
-            if (asteroids[i] > 0) {
+        for(int i=0;i<n;i++){
+            if(asteroids[i]>0){
                 st.push_back(asteroids[i]);
             }
-            else {
-                while (!st.empty() && st.back() > 0 && st.back() < abs(asteroids[i])) {
+            else{
+                while(!st.empty() && st.back() > 0 &&st.back() < abs(asteroids[i])){
                     st.pop_back();
                 }
-                if (!st.empty() && st.back() == abs(asteroids[i])) {
+                if(!st.empty() && st.back() == abs(asteroids[i])){
                     st.pop_back();
                 }
-                else if (st.empty() || st.back() < 0) {
+                else if(st.empty() || st.back()<0){
                     st.push_back(asteroids[i]);
                 }
             }
         }
-
-        // Convert list to vector
-        vector<int> result(st.begin(), st.end());
+        vector<int> result(st.begin(),st.end());
         return result;
     }
 };
