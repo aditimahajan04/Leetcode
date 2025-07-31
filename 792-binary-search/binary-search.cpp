e7fd08f1a6e7a925b13bf16c80d7e3c28e42cf7f
Aditi Mahajan <136177334+aditimahajan04@users.noreply.github.com>
2025-07-31 17:50:38 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (45.34%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,16 +4,17 @@ public:
         int n=nums.size();
         int low=0;
         int high=n-1;
+        
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]==target){
+            if(target==nums[mid]){
                 return mid;
             }
-            else if (nums[mid]<target){
-                low=mid+1;
+            else if(target<nums[mid]){
+                high=mid-1;
             }
             else{
-                high=mid-1;
+                low=mid+1;
             }
         }
         return -1;
